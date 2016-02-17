4127ecb756c5f9aa7beb366bfd0bbec91f5d9980
Dario Götz <dario.goetz@googlemail.com>
2016-02-17 18:12:53 +0100

    fix: threadsafe writing in statemanager object

--- statemanager/__init__.py
+++ statemanager/__init__.py
@@ -1,27 +1,32 @@
 from keys import *
-
+import threading
 
 class StateManager(object):
 
     def __init__(self):
+        self._lock = threading.Lock()
+
         self.active_tournaments = {}
         self.active_heats = {}
 
         #active_heats is dict: heat_id -> judge_id -> {info}
 
     def add_tournament(self, tournament):
-        self.active_tournaments[tournament.tournament_id] = tournament
+        with self._lock:
+            self.active_tournaments[tournament.tournament_id] = tournament
 
     def activate_heat(self, heat_id, heat_info):
         if heat_id is None or heat_info is None:
             return
         # heat info comes from web interface since database is not known here
-        self.active_heats[heat_id] = heat_info
+        with self._lock:
+            self.active_heats[heat_id] = heat_info
         return
 
     def deactivate_heat(self, heat_id):
-        if heat_id in self.active_heats:
-            del self.active_heats[heat_id]
+        with self._lock:
+            if heat_id in self.active_heats:
+                del self.active_heats[heat_id]
         return
 
     def get_active_heat_info(self, heat_id=None):
